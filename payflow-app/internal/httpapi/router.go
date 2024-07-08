2da47d10046aa381bdb0e127a3df253faf01d2a4
Jien8Huang <Huang.Jien@outlook.com>
2024-07-08 13:55:40 +0000

    feat: wire Chi router and routes

---
+++ payflow-app/internal/httpapi/router.go
@@ -0,0 +1,38 @@
+package httpapi
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+
+	"github.com/payflow/payflow-app/internal/auth"
+)
+
+func NewRouter(s *Server) chi.Router {
+	r := chi.NewRouter()
+	r.Use(middleware.Recoverer)
+	r.Use(RequestID)
+	r.Use(RequestLogger)
+
+	r.Post("/v1/tenants", s.postTenants)
+	r.Post("/v1/auth/login", s.postAuthLogin)
+
+	r.Route("/v1", func(r chi.Router) {
+		r.With(MeAuth(s.Pool, s.JWTSecret)).Get("/tenants/me", s.getTenantsMe)
+		r.With(MeAuth(s.Pool, s.JWTSecret)).Patch("/tenants/me/webhook", s.patchTenantWebhook)
+		r.With(MeAuth(s.Pool, s.JWTSecret)).Get("/webhook-deliveries", s.listWebhookDeliveries)
+		r.With(MeAuth(s.Pool, s.JWTSecret)).Get("/webhook-deliveries/{deliveryID}", s.getWebhookDelivery)
+		r.With(auth.MiddlewareAPIKey(s.Pool)).Post("/tenants/{tenantID}/dashboard-users", s.postDashboardUser)
+		r.With(auth.MiddlewareAPIKey(s.Pool)).Post("/payments", s.postPayment)
+		r.With(auth.MiddlewareAPIKey(s.Pool)).Get("/payments/{paymentID}", s.getPayment)
+		r.With(auth.MiddlewareAPIKey(s.Pool)).Post("/payments/{paymentID}/refunds", s.postRefund)
+		r.With(auth.MiddlewareAPIKey(s.Pool)).Get("/refunds/{refundID}", s.getRefund)
+	})
+
+	r.Get("/healthz", func(w http.ResponseWriter, _ *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		_, _ = w.Write([]byte("ok"))
+	})
+	return r
+}

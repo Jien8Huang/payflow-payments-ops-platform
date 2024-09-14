67d6fbe5dcb026bf56479b73903ea72455679ef6
Jiasong8Sun <jiasong.sun@outlook.com>
2024-09-14 02:56:16 +0000

    infra: add Azure network module (main.tf)

---
+++ payflow-terraform-modules/modules/azure_network/main.tf
@@ -0,0 +1,38 @@
+resource "azurerm_resource_group" "this" {
+  name     = "${var.name_prefix}-network-rg"
+  location = var.location
+  tags     = var.tags
+}
+
+resource "azurerm_virtual_network" "this" {
+  name                = "${var.name_prefix}-vnet"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.this.name
+  address_space       = var.address_space
+  tags                = var.tags
+}
+
+resource "azurerm_subnet" "aks" {
+  name                 = "${var.name_prefix}-aks"
+  resource_group_name  = azurerm_resource_group.this.name
+  virtual_network_name = azurerm_virtual_network.this.name
+  address_prefixes     = [var.aks_subnet_cidr]
+}
+
+resource "azurerm_subnet" "postgres" {
+  name                 = "${var.name_prefix}-postgres"
+  resource_group_name  = azurerm_resource_group.this.name
+  virtual_network_name = azurerm_virtual_network.this.name
+  address_prefixes     = [var.postgres_subnet_cidr]
+
+  delegation {
+    name = "postgres-delegation"
+
+    service_delegation {
+      name = "Microsoft.DBforPostgreSQL/flexibleServers"
+      actions = [
+        "Microsoft.Network/virtualNetworks/subnets/join/action",
+      ]
+    }
+  }
+}

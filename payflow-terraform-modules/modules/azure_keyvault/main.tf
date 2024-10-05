4b81338f5219d3ed3ce62533ace733593813cacc
Jiasong8Sun <jiasong.sun@outlook.com>
2024-10-05 11:55:40 +0000

    infra: add Azure Key Vault module (main.tf)

---
+++ payflow-terraform-modules/modules/azure_keyvault/main.tf
@@ -0,0 +1,15 @@
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_key_vault" "this" {
+  name                       = var.key_vault_name
+  location                   = var.location
+  resource_group_name        = var.resource_group_name
+  tenant_id                  = data.azurerm_client_config.current.tenant_id
+  sku_name                   = var.sku_name
+  soft_delete_retention_days = 7
+  purge_protection_enabled   = var.purge_protection_enabled
+
+  rbac_authorization_enabled = true
+
+  tags = var.tags
+}

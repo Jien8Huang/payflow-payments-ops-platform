4ab102d1234ab5b979f62860bfc87ae7da7162e9
Jien8Huang <Huang.Jien@outlook.com>
2026-04-21 12:01:13 -0400

    refactor(terraform): enhance formatting consistency in main.tf files across modules

--- payflow-terraform-modules/modules/azure_postgres/main.tf
+++ payflow-terraform-modules/modules/azure_postgres/main.tf
@@ -21,19 +21,19 @@ resource "azurerm_private_dns_zone_virtual_network_link" "postgres" {
 }
 
 resource "azurerm_postgresql_flexible_server" "this" {
-  name                   = "${var.name_prefix}-psql"
-  resource_group_name    = var.resource_group_name
-  location               = var.location
-  version                = var.postgres_version
-  delegated_subnet_id    = var.delegated_subnet_id
-  private_dns_zone_id    = azurerm_private_dns_zone.postgres.id
-  administrator_login    = var.administrator_login
-  administrator_password = var.administrator_password
-  sku_name                 = var.sku_name
-  storage_mb               = 32768
-  backup_retention_days    = 7
+  name                         = "${var.name_prefix}-psql"
+  resource_group_name          = var.resource_group_name
+  location                     = var.location
+  version                      = var.postgres_version
+  delegated_subnet_id          = var.delegated_subnet_id
+  private_dns_zone_id          = azurerm_private_dns_zone.postgres.id
+  administrator_login          = var.administrator_login
+  administrator_password       = var.administrator_password
+  sku_name                     = var.sku_name
+  storage_mb                   = 32768
+  backup_retention_days        = 7
   geo_redundant_backup_enabled = false
-  tags                     = var.tags
+  tags                         = var.tags
 
   depends_on = [azurerm_private_dns_zone_virtual_network_link.postgres]
 }

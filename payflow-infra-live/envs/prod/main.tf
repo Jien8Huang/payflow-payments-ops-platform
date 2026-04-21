cee47135650cae78c06fc2ecd6a0221407351a5d
Jiasong8Sun <jiasong.sun@outlook.com>
2026-04-21 11:45:31 -0400

    refactor(terraform): standardize formatting in main.tf files across environments

--- payflow-infra-live/envs/prod/main.tf
+++ payflow-infra-live/envs/prod/main.tf
@@ -1,6 +1,6 @@
 resource "random_password" "postgres" {
-  length           = 32
-  special          = true
+  length  = 32
+  special = true
   override_special = "!#%&*-_=+"
   min_lower        = 4
   min_upper        = 4
@@ -17,18 +17,18 @@ locals {
     ManagedBy   = "terraform"
     Project     = "payflow"
   })
-  kv_raw  = "pf${replace(var.name_prefix, "-", "")}${random_id.kv.hex}"
+  kv_raw = "pf${replace(var.name_prefix, "-", "")}${random_id.kv.hex}"
   kv_name = length(local.kv_raw) > 24 ? substr(local.kv_raw, 0, 24) : local.kv_raw
 }
 
 module "network" {
-  source              = "../../../payflow-terraform-modules/modules/azure_network"
-  name_prefix         = var.name_prefix
-  location            = var.location
-  address_space       = var.address_space
-  aks_subnet_cidr     = var.aks_subnet_cidr
+  source               = "../../../payflow-terraform-modules/modules/azure_network"
+  name_prefix          = var.name_prefix
+  location             = var.location
+  address_space        = var.address_space
+  aks_subnet_cidr      = var.aks_subnet_cidr
   postgres_subnet_cidr = var.postgres_subnet_cidr
-  tags                = local.tags
+  tags                 = local.tags
 }
 
 module "keyvault" {
@@ -41,15 +41,15 @@ module "keyvault" {
 }
 
 module "postgres" {
-  source                  = "../../../payflow-terraform-modules/modules/azure_postgres"
-  name_prefix             = var.name_prefix
-  location                = module.network.location
-  resource_group_name     = module.network.resource_group_name
-  delegated_subnet_id     = module.network.postgres_subnet_id
-  virtual_network_id      = module.network.virtual_network_id
-  administrator_password  = random_password.postgres.result
-  sku_name                = var.postgres_sku
-  tags                    = local.tags
+  source                 = "../../../payflow-terraform-modules/modules/azure_postgres"
+  name_prefix            = var.name_prefix
+  location               = module.network.location
+  resource_group_name    = module.network.resource_group_name
+  delegated_subnet_id    = module.network.postgres_subnet_id
+  virtual_network_id     = module.network.virtual_network_id
+  administrator_password = random_password.postgres.result
+  sku_name               = var.postgres_sku
+  tags                   = local.tags
 }
 
 module "aks" {
